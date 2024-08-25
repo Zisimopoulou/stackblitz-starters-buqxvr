da4b6429e3aa7eb03bef9c53efbae47c4357d7c9
Zisimopoulou <zisimopouloua@protonmail.com>
2024-08-25 16:38:57 +0300

    modify home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true, // Mark as standalone
   templateUrl: './home.component.html', // Point to the external HTML file
+  styleUrls: ['./home.component.scss'], // Link to SCSS file
 })
 export class HomeComponent {}
