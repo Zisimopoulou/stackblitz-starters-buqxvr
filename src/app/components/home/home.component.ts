5593c0a58f18e6d4d09f3f351c7081fc4e9d649c
Zisimopoulou <zisimopouloua@protonmail.com>
2024-08-25 17:06:02 +0300

    add components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true, // Mark as standalone
   templateUrl: './home.component.html', // Point to the external HTML file
   styleUrls: ['./home.component.scss'], // Link to SCSS file
+  imports: [FooterComponent], // Import the FooterComponent here
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Add this schema
 })
 export class HomeComponent {}
