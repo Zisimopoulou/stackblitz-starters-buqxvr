bad6598e7f336e3bed5a6d909ca8c34a57819da0
Zisimopoulou <zisimopouloua@protonmail.com>
2024-09-02 16:05:34 +0300

    redirect to about

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common'; // Import NgIf directly
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true, // Ensure the component is standalone if using this approach
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  imports: [RouterLink, RouterModule]
 })
 export class HeaderComponent {
   menuOpen = false; // State to track the menu visibility
