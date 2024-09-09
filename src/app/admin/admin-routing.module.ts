31670755a95f06aad3df501aa9b6bd86e6ba7a1c
Zisimopoulou <zisimopouloua@protonmail.com>
2024-09-09 11:51:23 +0300

    admin page

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,8 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';  // Import the standalone login component
 import { DashboardComponent } from './components/dashboard/dashboard.component';  // Import the standalone dashboard component
 import { AuthGuard } from '../auth.guard';
+import { AdminCategoryComponent } from './components/admin-category/admin-category.component';  // Import the standalone login component
+import { AdminProductComponent } from './components/admin-product/admin-product.component';  // Import the standalone login component
 
 const routes: Routes = [
+  { path: 'admin-category', component: AdminCategoryComponent },  // Route for login
+  { path: 'admin-product', component: AdminProductComponent },  // Route for login
   { path: 'login', component: LoginComponent },  // Route for login
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },  // Protected route
   { path: '', redirectTo: 'login', pathMatch: 'full' }  // Default route for admin module
--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.scss'],
+  standalone: true
 })
 export class DashboardComponent {
+  constructor(private router: Router) {}
 
+  goToCategories() {
+    this.router.navigate(['/admin/admin-category']);
+  }
+
+  goToProducts() {
+    this.router.navigate(['/admin/admin-product']);
+  }
 }
